429d0be3761c0552563b94cccbc112012f732c1f
Yağız <91464589+ktriacxx@users.noreply.github.com>
2022-09-23 18:28:59 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,24 @@
+﻿module.exports = {
+    tokens: {
+        moderation: "",
+        stat: ""
+    },
+    panels: {
+        defaultName: "• Kayıtsız",
+        suspeciousName: "• Yeni Hesap",
+        bio: ["ktria🐱‍👤"],
+        status: "idle",
+        prefix: "!",
+        botVoice: "",
+        mongoURL1: "",
+        guildID: "",
+        botOwner: ["711897884391637062"],
+        type: "LISTENING",
+        name: true,
+        banLimit: 3
+    },
+    emojis: {
+        onay: "👌",
+        red:"🚫"
+    }
+} 
